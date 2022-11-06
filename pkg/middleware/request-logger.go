61c8eeff56fe257fe7bc594f32863d49b15eb074
Philip Stefou <hashibuto@noreply.com>
2022-11-07 03:09:38 +0900

    improved request logger

--- pkg/middleware/request-logger.go
+++ pkg/middleware/request-logger.go
@@ -13,6 +13,10 @@ func RequestLogger(next neko.Handler) neko.Handler {
 		now := time.Now().UTC()
 		fmt.Printf("%s - %s %s\n", now.Format(time.RFC3339Nano), r.Method, r.URL.Path)
 		err := next.ServeHTTP(w, r)
+		if err != nil {
+			fmt.Printf("Error occurred during request\n%v\n", err)
+		}
+
 		statusCode := neko.GetStatusCode(w, err)
 		dur := time.Now().UTC().Sub(now).String()
 		fmt.Printf("%s - %s %s %d (%s)\n", now.Format(time.RFC3339Nano), r.Method, r.URL.Path, statusCode, dur)
