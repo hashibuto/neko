1ee625558f269b5b9628ef522be6b4c6998e22e3
Philip Stefou <hashibuto@noreply.com>
2022-11-09 05:33:45 +0900

    added remote address logging

--- pkg/middleware/request-logger.go
+++ pkg/middleware/request-logger.go
@@ -11,7 +11,7 @@ import (
 func RequestLogger(next neko.Handler) neko.Handler {
 	return neko.MakeHandler(func(w http.ResponseWriter, r *http.Request) error {
 		now := time.Now().UTC()
-		fmt.Printf("%s - %s %s\n", now.Format(time.RFC3339Nano), r.Method, r.URL.Path)
+		fmt.Printf("%s - %s %s %s\n", now.Format(time.RFC3339Nano), r.RemoteAddr, r.Method, r.URL.Path)
 		err := next.ServeHTTP(w, r)
 		if err != nil {
 			fmt.Printf("Error occurred during request\n%v\n", err)
@@ -19,7 +19,7 @@ func RequestLogger(next neko.Handler) neko.Handler {
 
 		statusCode := neko.GetStatusCode(w, err)
 		dur := time.Now().UTC().Sub(now).String()
-		fmt.Printf("%s - %s %s %d (%s)\n", now.Format(time.RFC3339Nano), r.Method, r.URL.Path, statusCode, dur)
+		fmt.Printf("%s - %s %s %s %d (%s)\n", now.Format(time.RFC3339Nano), r.RemoteAddr, r.Method, r.URL.Path, statusCode, dur)
 
 		return err
 	})
